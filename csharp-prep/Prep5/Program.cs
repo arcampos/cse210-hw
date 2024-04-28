602e3cc6cbd47ef493d813dfa129a0cae2e7b9ca
arcampos <arnold.campos.ar@gmail.com>
2024-04-28 19:33:28 -0400

    5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PrompUserName();
+        int userNumber = PrompUserNumber();
+
+        int squaredNumber = SquareNumber (userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Squared Number Calculator!");
+    }
+
+    static string PrompUserName()
+    {
+        Console.WriteLine("What is your name ?");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PrompUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine(userName + ", your number squared is: " + squaredNumber);
     }
 }
