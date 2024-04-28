93a5221ec50a3d6b0934ec31e0dde2a9b5dc2ddd
arcampos <arnold.campos.ar@gmail.com>
2024-04-28 19:06:28 -0400

    3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Part 1 and 2
+        // Console.Write("What is the magic number?");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        //Part 3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess!= magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
+
     }
 }
